e8d043a36406d33a80b11d6df515f637f4b3038d
CtrlTistix <77314210+CtrlTistix@users.noreply.github.com>
2025-11-09 21:03:25 -0500

    Eliminacion de ConfigPage

--- Backend/app/model/__init__.py
+++ Backend/app/model/__init__.py
@@ -1,3 +1,3 @@
-from app.models import User, Session, AuditLog, UserRole, ActionType
+# from app.models import User, Session, AuditLog, UserRole, ActionType
 
-__all__ = ['User', 'Session', 'AuditLog', 'UserRole', 'ActionType']
+# __all__ = ['User', 'Session', 'AuditLog', 'UserRole', 'ActionType']
